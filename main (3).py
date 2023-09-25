465f0f1317cab6d533ec24b94c54142567f3b46d
asbdu263cb22s614403 <144351535+asbdu263cb22s614403@users.noreply.github.com>
2023-09-25 21:17:51 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,13 @@
+class player:
+  def play(self):
+     print("jegathish neepalying for free fire.")
+class Alok(player):
+  def play(self):
+      print("alok is a active skill")
+class Kelly(player):
+  def play(self):
+      print("kelly is skill in run fast")
+alok=Alok()
+kelly=Kelly()
+alok.play()
+.kelly.play()
