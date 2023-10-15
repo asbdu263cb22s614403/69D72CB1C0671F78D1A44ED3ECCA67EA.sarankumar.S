135451c5a6ffa9bd3fc9fcc4e35a9fb8ca986181
asbdu263cb22s614403 <144351535+asbdu263cb22s614403@users.noreply.github.com>
2023-10-15 08:57:47 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,11 @@
+# Quick examples to convert tuple to list
+
+# Example 1: Convert the tuple to a list
+tuples = (0, 2, 4, 6, 8)
+mylist = list(tuples)
+
+# Example 2: Convert the tuple to a list 
+# Using * unpacking method
+tuples = ('Spark', 'Python', 'pandas', 'Java')
+# convert tuple into list
+list1 = [*tuples,]
---
+++ main (7).py
@@ -0,0 +1,12 @@
+L1=[10,30,50,20,40]
+T1=(10,50,30,40,20)
+print (len(L1))
+print (len(T1))
+print ('max of L1', max(L1))
+print ('max of T1', max(T1))
+print ('min of L1', min(L1))
+print ('min of T1', min(T1))
+print ('sum of L1', sum(L1))
+print ('sum of T1', sum(T1))
+print ('L1 in sorted order', sorted(L1))
+print ('T1 in sorted order', sorted(T1))
